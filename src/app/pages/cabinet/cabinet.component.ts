02d16c0c930fa1ee22c82b559ffd30b00bb042b8
danyloposhenov <dendicuz01@gmail.com>
2023-03-20 20:33:34 +0200

    added modal forms on header

---
+++ src/app/pages/cabinet/cabinet.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AccountService } from 'src/app/shared/services/account/account.service';
+
+@Component({
+  selector: 'app-cabinet',
+  templateUrl: './cabinet.component.html',
+  styleUrls: ['./cabinet.component.scss']
+})
+export class CabinetComponent {
+
+  constructor(
+    private accountService: AccountService,
+    private router: Router
+  ) {}
+
+
+  logOut(): void {
+    this.router.navigate(['/'])
+    localStorage.removeItem('currentUser');
+    this.accountService.isUserLogin$.next(true);
+  }
+}
